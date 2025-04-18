8e717e321eb9e8317627bb637dc30ed07689bfac
elan-sk <elan-sk@hotmail.com>
2025-04-18 15:28:37 -0500

    Primeros pasos

---
+++ Devtree/src/router.ts
@@ -0,0 +1,9 @@
+import { Router } from "express"
+import { createAccount } from "./handlers"
+
+const router = Router()
+
+/** Autenticación y Registro */
+router.post('/auth/register', createAccount )
+
+export default router
